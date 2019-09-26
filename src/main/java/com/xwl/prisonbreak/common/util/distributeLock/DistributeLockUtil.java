386e50124960e24e707c8e6eb2269009d164f449
xuning <978100228@qq.com>
2019-09-26 18:15:45 +0800

    zookeeper实现分布式锁工具类

--- src/main/java/com/xwl/prisonbreak/common/util/distributeLock/DistributeLockUtil.java
+++ src/main/java/com/xwl/prisonbreak/common/util/distributeLock/DistributeLockUtil.java
@@ -21,15 +21,16 @@ public class DistributeLockUtil {
     // zookeeper客户端
     private CuratorFramework client = null;
 
+    // 日志
     private final Logger log = LoggerFactory.getLogger(DistributeLockUtil.class);
 
     // 用于挂起当前请求，并且等待上一个分布式锁释放
     private static CountDownLatch zkLockLatch = new CountDownLatch(1);
 
     // 分布式锁的总结点名
-    private static final String ZK_LOCKP_ROJECT = "zk-locks";
+    private static final String ZK_LOCK_PROJECT = "zk-locks";
 
-    // 分布式锁节点
+    // “当前业务”的分布式锁节点，不同的业务需要不同的分布式锁
     private static final String DISTRIBUTED_LOCK = "distributed-lock";
 
     /**
@@ -49,24 +50,30 @@ public class DistributeLockUtil {
         client = client.usingNamespace("ZKLocks-Namespace");
 
         /**
-         * 创建zk的总结点
+         * 在命名空间下创建zk的总结点
          * ZKLocks-Namespace
          *     |
          *     --zk-locks
          *          |
-         *          --distributed-lock
+         *          --distributed-lock1 // 不同业务需要不同的分布式锁
+         *          --distributed-lock2 // 不同业务需要不同的分布式锁
+         *          --distributed-lock3 // 不同业务需要不同的分布式锁
+         *          ...
          *
          */
         try {
-            if (client.checkExists().forPath("/" + ZK_LOCKP_ROJECT) == null) {
+            // 在zookeeper第一次启动时要去创建分布式锁的总节点
+            if (client.checkExists().forPath("/" + ZK_LOCK_PROJECT) == null) {
+                // 创建分布式锁的总结点：zk-locks
                 client.create()
-                        .creatingParentsIfNeeded()
-                        .withMode(CreateMode.PERSISTENT)
-                        .withACL(ZooDefs.Ids.OPEN_ACL_UNSAFE)
-                        .forPath("/" + ZK_LOCKP_ROJECT);
+                        .creatingParentsIfNeeded() // 递归创建，这里也可以不要，因为ZK_LOCK_PROJECT是一个单节点，不存在父子关系
+                        .withMode(CreateMode.PERSISTENT) // 创建永久性节点
+                        .withACL(ZooDefs.Ids.OPEN_ACL_UNSAFE) // ACL权限
+                        .forPath("/" + ZK_LOCK_PROJECT); // 在client的"/"路径下创建，实际上是：ZKLocks-Namespace/zk-locks
             }
-            // 针对zk的分布式锁节点，创建相应的watcher事件监听
-            addWatcherToLock("/" + ZK_LOCKP_ROJECT);
+            // 针对zk的分布式锁节点，因为使用的是Curator框架，需要给“总结点”创建相应的watcher事件监听
+            // 如果是使用原生的zookeeper实现分布式锁，则应该给“具体的锁”（distributed-lock）创建watcher事件监听
+            addWatcherToLock("/" + ZK_LOCK_PROJECT);
         } catch (Exception e) {
             log.error("客户端连接zookeeper服务器出错...请重试！");
         }
@@ -79,21 +86,22 @@ public class DistributeLockUtil {
         // 使用死循环，当且仅当上一个锁释放并且当请求获得锁成功后才会跳出循环
         while (true) {
             try {
+                // 创建当前的业务锁:distributed-lock
                 client.create()
-                        .creatingParentsIfNeeded()
-                        .withMode(CreateMode.EPHEMERAL)
-                        .withACL(ZooDefs.Ids.OPEN_ACL_UNSAFE)
-                        .forPath("/" + ZK_LOCKP_ROJECT + "/" + DISTRIBUTED_LOCK);
+                        .creatingParentsIfNeeded() // 递归的方式创建业务锁
+                        .withMode(CreateMode.EPHEMERAL) // 创建“临时”节点，必须是临时节点，当会话断开，当前锁自动失效
+                        .withACL(ZooDefs.Ids.OPEN_ACL_UNSAFE) // ACL权限
+                        .forPath("/" + ZK_LOCK_PROJECT + "/" + DISTRIBUTED_LOCK); // 在client的"/ZKLocks-Namespace/zk-locks"路径下创建，实际上是：ZKLocks-Namespace/zk-locks/distributed-lock
                 log.info("获取分布式锁成功...");
                 return;
-            } catch (Exception e) {
+            } catch (Exception e) { // 如果这个当前业务的锁“已存在”，则说明有人正在使用这把锁，抛出异常
                 log.info("获取分布式锁失败...");
                 try {
                     // 如果没有获取到锁，需要重新设置同步资源值
                     if (zkLockLatch.getCount() <= 0) {
                         zkLockLatch = new CountDownLatch(1);
                     }
-                    // 阻塞线程
+                    // 阻塞线程，使当前请求挂起，直到占用这把锁的线程释放
                     zkLockLatch.await();
                 } catch (InterruptedException ex) {
                     ex.printStackTrace();
@@ -102,10 +110,14 @@ public class DistributeLockUtil {
         }
     }
 
+    /**
+     * 释放锁：就是删除节点
+     * @return
+     */
     public boolean releaseLock() {
         try {
-            if (client.checkExists().forPath("/" + ZK_LOCKP_ROJECT + "/" + DISTRIBUTED_LOCK) != null) {
-                client.delete().forPath("/" + ZK_LOCKP_ROJECT + "/" + DISTRIBUTED_LOCK);
+            if (client.checkExists().forPath("/" + ZK_LOCK_PROJECT + "/" + DISTRIBUTED_LOCK) != null) {
+                client.delete().forPath("/" + ZK_LOCK_PROJECT + "/" + DISTRIBUTED_LOCK);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -117,17 +129,22 @@ public class DistributeLockUtil {
 
     /**
      * 创建watcher监听
-     * @param path 路径
+     * @param path 路径：监听的总结点
      */
     private void addWatcherToLock(String path) throws Exception {
         final PathChildrenCache cache = new PathChildrenCache(client, path, true);
+        // 启动模式
         cache.start(PathChildrenCache.StartMode.POST_INITIALIZED_EVENT);
+        // 添加监听器
         cache.getListenable().addListener(new PathChildrenCacheListener() {
             @Override
             public void childEvent(CuratorFramework client, PathChildrenCacheEvent event) throws Exception {
+                // 当接收到一个事件watcher后，首先要去判断该事件的类型：event.getType()
                 if (event.getType().equals(PathChildrenCacheEvent.Type.CHILD_REMOVED)) {
+                    // 因为有不同的业务锁，所以需要获取该锁的路径
                     String path = event.getData().getPath();
                     log.info("上一个会话已释放锁或该会话已断开，节点路径为：" + path);
+                    // 获取路径后需要判断跟当前业务锁指定的名称是否相同
                     if (path.contains(DISTRIBUTED_LOCK)) {
                         log.info("释放计数器，让当前请求来获取分布式锁...");
                         zkLockLatch.countDown();
